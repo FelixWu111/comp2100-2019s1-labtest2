3532c9e72ef9a4757ae6cb78fd7e2816bf291fef
Duy Khuu <u6380923@anu.edu.au>
2019-05-18 13:12:11 +1000

    Update C1 imports

--- C1 LIS/longest_increasing_sequence.java
+++ C1 LIS/longest_increasing_sequence.java
@@ -1,6 +1,3 @@
-import org.junit.Assert;
-import org.testng.annotations.Test;
-
 public class longest_increasing_sequence {
     //return the length of longest increasing subsequence of the given sequence
     public int LIS(int[] a) {
